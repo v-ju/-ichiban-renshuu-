75fc145fb3ae8ad2780715917551ec1bdd4533d3
Jaya <jaya.dev100x@gmail.com>
2025-10-15 23:46:52 +0530

    basic landing page init

--- apps/back/index.js
+++ apps/back/index.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import prisma from '@repo/db/prisma'
 import 'dotenv/config'
+import router from './routes'
+
 const app = express()
 app.use(express.json())
 
+app.use('/api/v1',router)
+
 const PORT = process.env.PORT
 console.log(PORT)
+
 async function connectDB(PORT) {
     try{
         await prisma.$connect()
